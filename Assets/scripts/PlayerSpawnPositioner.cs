b846176674bf2995fd4948c0100635de929a9b1f
kenzyhamed <kenzy.hamed@ucalgary.ca>
2026-09-18 14:06:30 -0600

    dhdehdu

--- Assets/scripts/PlayerSpawnPositioner.cs
+++ Assets/scripts/PlayerSpawnPositioner.cs
@@ -6,12 +6,8 @@ public class PlayerSpawnPositioner : MonoBehaviour
     [SerializeField] private Transform startSpot;
 
 
-
     public void MoveToStartSpot()
     {
-
-        if (startSpot == null)
-            startSpot = GameObject.FindWithTag("StartSpot")?.transform;
 
         if (startSpot != null)
             transform.SetPositionAndRotation(startSpot.position, startSpot.rotation);
